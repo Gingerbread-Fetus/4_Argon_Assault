5fba318510a78abc869ae591c5131fdfb09406ae
Hayden Avery Matthews <haydenamatthews@yahoo.com>
2020-04-08 07:04:57 -0600

    -Implemented the scoreboard -Moved the scoring onto the enemy script

--- 4_Argon_Assault/Assets/Scripts/ScoreBoard.cs
+++ 4_Argon_Assault/Assets/Scripts/ScoreBoard.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 
 public class ScoreBoard : MonoBehaviour
 {
-    [SerializeField] int scorePerHit = 12;
 
     int score;
     TextMeshProUGUI scoreText;
@@ -16,9 +15,9 @@ public class ScoreBoard : MonoBehaviour
         scoreText.text = score.ToString();
     }
 
-    public void ScoreHit()
+    public void ScoreHit(int scoreIncrease)
     {
-        score = score + scorePerHit;
+        score = score + scoreIncrease;
         scoreText.text = score.ToString();
     }
 }
